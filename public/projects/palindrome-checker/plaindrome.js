728dd09005ca05937db2856db3d46f6c9dddbae4
Andrew Jana <157134333+VGAisdead@users.noreply.github.com>
2024-12-28 22:16:30 +0100

    Add files via upload

--- public/projects/palindrome-checker/plaindrome.js
+++ public/projects/palindrome-checker/plaindrome.js
@@ -2,6 +2,7 @@
 
 const input = document.getElementById("text-input");
 const result = document.getElementById("result");
+const isHungarian = document.getElementById("isHungarian");
 
 function checkInput() {
   if (input.value === "") {
@@ -14,11 +15,29 @@ function checkInput() {
 function checkPalindrome(str) {
   const re = /[\W_]/g;
   const lowStr = str.toLowerCase().replace(re, "");
-  const rev = lowStr.split("").reverse().join("");
+  let normalizedStr = lowStr;
 
-  if (rev === lowStr) {
-    result.innerHTML = `A(z) <strong>${input.value}</strong> egy palindrom.`;
+  if (isHungarian.checked) {
+    // Magyar nyelvi sajátosságok
+    normalizedStr = normalizedStr
+      .replace(/cs/g, "§") // Az "cs"-t egy speciális karakterre cseréljük
+      .replace(/dz/g, "¤") // Az "dz"-t egy másik speciális karakterre cseréljük
+      .replace(/dzs/g, "¥") // Az "dzs"-t egy harmadik karakterre cseréljük
+      .replace(/gy/g, "§§") // Az "gy"-t egy másik speciális karakterre cseréljük
+      .replace(/ly/g, "¤¤") // Az "ly"-t egy másik karakterre
+      .replace(/ny/g, "¥¥") // Az "ny"-t egy új karakterre
+      .replace(/sz/g, "∑") // Az "sz"-t egy másik karakterre
+      .replace(/ty/g, "∞"); // Az "ty"-t egy másik karakterre
+  }
+
+  const rev = normalizedStr.split("").reverse().join("");
+
+  console.log(lowStr);
+  console.log(rev);
+
+  if (rev === normalizedStr) {
+    result.innerHTML = `A(z) "<strong>${input.value}</strong>" egy palindrom.`;
   } else {
-    result.innerHTML = `A(z) <strong>${input.value}</strong> nem palindrom.`;
+    result.innerHTML = `A(z) "<strong>${input.value}</strong>" nem palindrom.`;
   }
 }
