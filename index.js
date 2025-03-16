7d04137983872c3d67657543117ddf23af2d4d0d
Jana András <beemyhoe@gmail.com>
2025-03-17 00:23:43 +0100

    changes

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const whoami = document.querySelector(".whoami");
 
-const careers = ["kóder ", "könyvelő ", "gamer ", "örök tanuló! "];
+const careers = [
+	"kóder ",
+	"könyvelő ",
+	"gamer ",
+	"fejlesztő ",
+	"örök tanuló! ",
+];
 let careerIndex = 0;
 let characterIndex = 0;
 
 function updateCareer() {
-  characterIndex++;
-  whoami.innerHTML = `
+	characterIndex++;
+	whoami.innerHTML = `
 <h1 class="fw-bold text-body-emphasis mb-3 whoami">Üdvözöllek!<br>
 András vagyok - <br>${careers[careerIndex].slice(0, characterIndex)}</h1>`;
 
-  if (characterIndex === careers[careerIndex].length) {
-    careerIndex++;
-    characterIndex = 0;
-  }
+	if (characterIndex === careers[careerIndex].length) {
+		careerIndex++;
+		characterIndex = 0;
+	}
 
-  if (careerIndex === careers.length) {
-    setTimeout(() => {
-      careerIndex = 0;
-      updateCareer();
-    }, 10000);
-  }
-  setTimeout(updateCareer, 300);
+	if (careerIndex === careers.length) {
+		setTimeout(() => {
+			careerIndex = 0;
+			updateCareer();
+		}, 10000);
+	}
+	setTimeout(updateCareer, 300);
 }
 
 updateCareer();
