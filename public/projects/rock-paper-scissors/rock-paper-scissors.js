d5ad2aeac520d04996cf57d590a8812bf78a8f41
Jana András <beemyhoe@gmail.com>
2025-05-04 01:25:12 +0200

    lol

--- public/projects/rock-paper-scissors/rock-paper-scissors.js
+++ public/projects/rock-paper-scissors/rock-paper-scissors.js
@@ -9,63 +9,67 @@ const optionsContainer = document.querySelector(".options-container");
 const resetGameBtn = document.getElementById("reset-game-btn");
 
 function getRandomComputerResult() {
-  const options = ["Kő", "Papír", "Olló"];
-  const randomIndex = Math.floor(Math.random() * options.length);
-  return (computerChoice = options[randomIndex]);
+	const options = ["Kő", "Papír", "Olló"];
+	const randomIndex = Math.floor(Math.random() * options.length);
+	return (computerChoice = options[randomIndex]);
 }
 
 function hasPlayerWonTheRound(player, computer) {
-  return (
-    (player === "Kő" && computer === "Olló") ||
-    (player === "Olló" && computer === "Papír") ||
-    (player === "Papír" && computer === "Kő")
-  );
+	return (
+		(player === "Kő" && computer === "Olló") ||
+		(player === "Olló" && computer === "Papír") ||
+		(player === "Papír" && computer === "Kő")
+	);
 }
 
 function getRoundResults(userOption) {
-  const computerResult = getRandomComputerResult();
+	const computerResult = getRandomComputerResult();
 
-  if (hasPlayerWonTheRound(userOption, computerResult)) {
-    playerScore++;
-    return `Győztél!`;
-  } else if (computerResult === userOption) {
-    return `Döntetlen!`;
-  } else {
-    computerScore++;
-    return `Vesztettél!`;
-  }
+	if (hasPlayerWonTheRound(userOption, computerResult)) {
+		playerScore++;
+		return `Győztél!`;
+	} else if (computerResult === userOption) {
+		return `Döntetlen!`;
+	} else {
+		computerScore++;
+		return `Vesztettél!`;
+	}
 }
 
 function showResults(userOption) {
-  roundResultsMsg.innerText = getRoundResults(userOption);
-  computerScoreSpanElement.innerText = computerScore;
-  playerScoreSpanElement.innerText = playerScore;
+	roundResultsMsg.innerText = getRoundResults(userOption);
+	computerScoreSpanElement.innerText = computerScore;
+	playerScoreSpanElement.innerText = playerScore;
 
-  if (playerScore === 3) {
-    winnerMsgElement.innerText = `Sikeresen legyőzted a számítógépet!
+	if (playerScore === 3) {
+		winnerMsgElement.innerText = `Sikeresen legyőztél egy nem túlképzett algoritmust,
+    azonban ezzel bizonyítottad rátermettséged a digitális világháború korában.
+    Most talán nem mentetted meg a világot, de egyszer talán eljön annak is az ideje...
     Gratulálok!`;
-    resetGameBtn.style.display = "block";
-    optionsContainer.style.display = "none";
-  } else if (computerScore === 3) {
-    winnerMsgElement.innerText = ` A számítógép győzött.`;
-    resetGameBtn.style.display = "block";
-    optionsContainer.style.display = "none";
-  }
-  playerHand.classList.remove("hidden");
-  computerHand.classList.remove("hidden");
+		resetGameBtn.style.display = "block";
+		optionsContainer.style.display = "none";
+	} else if (computerScore === 3) {
+		winnerMsgElement.innerText = ` Ezennel a Skynet győzedelmeskedett az ember felett,
+     szeretett világunk ismét közelebb került a pusztuláshoz,
+      s a 100%-ban digitális társadalom beköszöntéséhez.`;
+		resetGameBtn.style.display = "block";
+		optionsContainer.style.display = "none";
+	}
+	playerHand.classList.remove("hidden");
+	computerHand.classList.remove("hidden");
 }
 
 function resetGame() {
-  playerScore = 0;
-  computerScore = 0;
-  computerScoreSpanElement.innerText = computerScore;
-  playerScoreSpanElement.innerText = playerScore;
-  resetGameBtn.style.display = "none";
-  optionsContainer.style.display = "flex";
-  winnerMsgElement.innerText = "";
-  roundResultsMsg.innerText = "";
-  playerHand.classList.add("hidden");
-  computerHand.classList.add("hidden");
+	playerScore = 0;
+	computerScore = 0;
+	computerScoreSpanElement.innerText = computerScore;
+	playerScoreSpanElement.innerText = playerScore;
+	resetGameBtn.style.display = "none";
+	optionsContainer.style.display = "flex";
+	winnerMsgElement.innerText = "";
+	roundResultsMsg.innerText = "";
+	playerHand.classList.add("hidden");
+	computerHand.classList.add("hidden");
 }
 
 resetGameBtn.addEventListener("click", resetGame);
@@ -75,18 +79,18 @@ const paperBtn = document.getElementById("paper-btn");
 const scissorsBtn = document.getElementById("scissors-btn");
 
 rockBtn.addEventListener("click", function () {
-  showResults("Kő");
-  koPapirOllo("Kő", computerChoice);
+	showResults("Kő");
+	koPapirOllo("Kő", computerChoice);
 });
 
 paperBtn.addEventListener("click", function () {
-  showResults("Papír");
-  koPapirOllo("Papír", computerChoice);
+	showResults("Papír");
+	koPapirOllo("Papír", computerChoice);
 });
 
 scissorsBtn.addEventListener("click", function () {
-  showResults("Olló");
-  koPapirOllo("Olló", computerChoice);
+	showResults("Olló");
+	koPapirOllo("Olló", computerChoice);
 });
 
 //  ANIMATION  //
@@ -95,20 +99,20 @@ const computerHand = document.getElementById("szamitogepvalasztas");
 
 // Animáció funkció
 function koPapirOllo(playerChoice, computerChoice) {
-  const choicesMap = {
-    Kő: "rock",
-    Papír: "paper",
-    Olló: "scissors",
-  };
+	const choicesMap = {
+		Kő: "rock",
+		Papír: "paper",
+		Olló: "scissors",
+	};
 
-  playerHand.src = `../../../assets/images/rockpaperscissors/player-${choicesMap[playerChoice]}.png`;
-  computerHand.src = `../../../assets/images/rockpaperscissors/computer-${choicesMap[computerChoice]}.png`;
+	playerHand.src = `../../../assets/images/rockpaperscissors/player-${choicesMap[playerChoice]}.png`;
+	computerHand.src = `../../../assets/images/rockpaperscissors/computer-${choicesMap[computerChoice]}.png`;
 
-  playerHand.classList.add("balkez-animacio");
-  computerHand.classList.add("jobbkez-animacio");
+	playerHand.classList.add("balkez-animacio");
+	computerHand.classList.add("jobbkez-animacio");
 
-  setTimeout(() => {
-    playerHand.classList.remove("balkez-animacio");
-    computerHand.classList.remove("jobbkez-animacio");
-  }, 1000);
+	setTimeout(() => {
+		playerHand.classList.remove("balkez-animacio");
+		computerHand.classList.remove("jobbkez-animacio");
+	}, 1000);
 }
