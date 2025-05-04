2309ff2b801678e6e24a9a69a519ca18f4d310ae
Jana András <beemyhoe@gmail.com>
2025-05-04 15:31:42 +0200

    updates

--- public/projects/weather-app/weather-app.js
+++ public/projects/weather-app/weather-app.js
@@ -1,3 +1,19 @@
 "use strict";
 
 const searchBtn = document.getElementById("searchBtn");
+const inputBtn = document.getElementById("inputBtn");
+const startModal = document.getElementById("start-modal");
+
+// Induló modal megjelenítése
+document.addEventListener("DOMContentLoaded", () => {
+	if (startModal) startModal.classList.add("show");
+});
+
+// Keresés
+inputBtn.addEventListener("click", () => {
+	startModal.classList.toggle("show");
+});
+
+searchBtn.addEventListener("click", () => {
+	startModal.classList.toggle("show");
+});
